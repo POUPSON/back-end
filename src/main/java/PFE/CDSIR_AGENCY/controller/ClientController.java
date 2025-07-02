e93f20f2274a006e627a44616712fb06c28f20b9
POUPSON <165962579+POUPSON@users.noreply.github.com>
2025-07-02 09:52:10 +0100

    Update ClientController.java

--- src/main/java/PFE/CDSIR_AGENCY/controller/ClientController.java
+++ src/main/java/PFE/CDSIR_AGENCY/controller/ClientController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://127.0.0.1:4200"}) // ✅ AJOUT
 
